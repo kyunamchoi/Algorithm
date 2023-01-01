c7de0e8ff4f0e993d90333c02fc97f3d833eb4f5
kyunamchoi <tfkorea@kakao.com>
2023-01-01 21:28:52 +0900

    update problem files

--- sorting/10989.py
+++ sorting/10989.py
@@ -1,5 +1,20 @@
+import sys
+n = int(sys.stdin.readline().rstrip())
+count = [0] * 10001
+
+for i in range(n):
+    num = int(sys.stdin.readline().rstrip())
+    count[num] += 1
+for i in range(10001):
+    for _ in range(count[i]):
+        print(i)
+#######################################################
+"""
+같은 알고리즘을 사용하더라도 arr를 만들고, append 하는 과정을 최소로. 메모리 사용 down
+"""
+#######################################################
 def counting_sort(arr):
-    max_arr = max(arr)
+    max_arr = 10000
     count = [0] * (max_arr+1)
     sorted_arr = list()
 
@@ -10,13 +25,10 @@ def counting_sort(arr):
         for _ in range(count[i]):
             sorted_arr.append(i)
     return sorted_arr
-
-n = int(input())
-arr = []
+import sys
+n = int(sys.stdin.readline().rstrip())
+arr = list()
 for i in range(n):
-    arr.append(int(input()))
-
-sorted_arr = counting_sort(arr)
-
-for i in sorted_arr:
+    arr.append(int(sys.stdin.readline().rstrip()))
+for i in counting_sort(arr):
     print(i)
